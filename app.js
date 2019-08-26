19093fd099cbb3316ba3c0637ae85981b964eba8
trrstnn <tristan.declarin@gmail.com>
2019-08-26 05:41:07 -0700

    adds styling completes functionality

--- app.js
+++ app.js
@@ -1,63 +1,94 @@
-const userssUrl = 'http://api.open-notify.org/astros.json';
-// const wikiUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/';
+const usersUrl = 'https://randomuser.me/api/?results=12';
 const gallery = document.getElementById('gallery');
 const btn = document.querySelector('button');
 
-// function getJSON(url) {
-//   return new Promise((resolve,reject)=>{
-//   const xhr = new XMLHttpRequest();
-//   xhr.open('GET', url);
-//   xhr.onload = () => {
-//     if(xhr.status === 200) {
-//       let data = JSON.parse(xhr.responseText);
-//       resolve(data);
-//     }else{
-//       reject(Error(xhr.statusText))
-//     }
-//   };
-//   xhr.onerror = () => reject(Error("A network error occured!"))
-//   xhr.send();
-//   });
-// }
 
-// function getProfiles(json) {
-//   const profiles = json.people.map( person => {
-//     return getJSON(wikiUrl + person.name, generateHTML);      
-//   }); 
-//   return profiles
-// }
 
-function generateHTML(data) {
-  data.map(person =>{ 
-    const cardDiv = document.createElement('div');
-    const imgContainer = document.createElement('div');
-    const infoContainer = document.createElement('div');
-    gallery.appendChild(cardDiv);
-    cardDiv.appendChild(imgContainer);
-    cardDiv.appendChild(infoContainer);
-    cardDiv.classList.add('card');
-    imgContainer.classList.add("card-img-container");
-    infoContainer.classList.add("card-info-container");
+//Fetches data and parses the json object
+async function getEmployees(url) {
+    const employeeResponse = await fetch(url);
+    const employeeJSON = await employeeResponse.json();
+    return Promise.all(employeeJSON.results);
+}
+  
 
-    section.innerHTML = `
-      <img src=${person.thumbnail.source}>
-      <h2>${person.title}</h2>
-      <p>${person.description}</p>
-      <p>${person.extract}</p>
-    `;
+function createEmployeeCard(data) {
+    data.map(person =>{ 
+        const cardDiv = document.createElement('div');
+        const imgContainer = document.createElement('div');
+        const infoContainer = document.createElement('div');
+        gallery.appendChild(cardDiv);
+        cardDiv.appendChild(imgContainer);
+        cardDiv.appendChild(infoContainer);
+        cardDiv.classList.add('card');
+        imgContainer.classList.add("card-img-container");
+        infoContainer.classList.add("card-info-container");
+
+        imgContainer.innerHTML = `<img class="card-img" src=${person.picture.large} alt="profile picture">`;
+        
+        infoContainer.innerHTML = `
+            <h3 id="name" class="card-name cap">${person.name.first} ${person.name.last} </h3>
+            <p class="card-text">${person.email}</p>
+            <p class="card-text cap">${person.location.city}, ${person.location.state}</p>
+            
+            `;
+            console.log(data)
   })
+  return data;
   
 }
-
-btn.addEventListener('click', (event) => {
-  event.target.textContent ="Loading...";
-  fetch(usersUrl)
-    .then(getProfiles)
-    .then(generateHTML)
-    .catch(err => {
-    peopleList.innerHTML = `${err}`
-    console.log(err);
-    })
-    .finally(()=>event.target.remove())
+//Closes the selected modal
+function closeModal() {
+  $('#modal-close-btn').click(() => {
+    $('.modal-container').slideUp(() => {
+      $('.modal-container').remove();
+    });
+  });
+}
+//Formats the date for birthday
+function dateFormatter(date) {
+    const retrievedDate = new Date(date);
+    const formatedDate = `${retrievedDate.getMonth() +1}/${retrievedDate.getDate()}/${retrievedDate.getFullYear()}`;
+    return formatedDate;
+  }
   
-});
+//Creates the modal when a specific employee is selected
+function viewEmployee(data) {
+  const employeeCards = document.querySelectorAll('.card');
+  for(let i = 0; i < data.length; i++){
+    employeeCards[i].addEventListener('click', () => {
+        console.log("click")
+      $('body').append(
+        `
+        <div class="modal-container">
+            <div class="modal">
+                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                <div class="modal-info-container">
+                    <img class="modal-img" src="${data[i].picture.large}" alt="profile picture">
+                    <h3 id="name" class="modal-name cap">${data[i].name.first} ${data[i].name.last}</h3>
+                    <p class="modal-text">${data[i].email}</p>
+                    <p class="modal-text cap">${data[i].location.city}</p>
+                    <hr>
+                    <p class="modal-text">${data[i].cell}</p>
+                    <p class="modal-text cap">${data[i].location.street}, ${data[i].location.city}, ${data[i].location.state} ${data[i].location.postcode}</p>
+                    <p class="modal-text">Birthday: ${dateFormatter(data[i].dob.date)}</p>
+                </div>
+            </div>
+        `
+      );
+      closeModal();
+      });
+  }
+  return data;
+}
+     getEmployees(usersUrl)
+      .then(createEmployeeCard)
+      .then(viewEmployee)
+      .catch(err => {
+      gallery.innerHTML = `${err}`
+      console.log(err);
+      })
+      
+
+
+
